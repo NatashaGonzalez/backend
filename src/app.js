410a5400591b1fe86331c417026772c8ca1a9dd9
NatashaGonzalez <gl5262992@gmail.com>
2023-08-12 22:39:39 +0100

    detalles

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import CartsRouter from "./routes/CartsRouter.js";
 
 const app = express ()
 
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
